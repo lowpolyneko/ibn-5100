bcb70fb8a8db8c0c7a245309753c1bac7d04de56
Ethan Wong <ewong@anl.gov>
2025-12-02 13:13:46 -0600

    packages: add discord

--- configuration.nix
+++ configuration.nix
@@ -2,8 +2,14 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    (discord.override {
+      withVencord = true;
+    })
     wezterm
   ];
+
+  # Allow unfree.
+  nixpkgs.config.allowUnfree = true;
 
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
